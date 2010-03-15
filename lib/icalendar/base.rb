91c0a007004ce7fe0a35cdd2f8106b36a9e42607
Sean Dague <sean@dague.net>
2010-03-15 08:37:43 -0400

    make 1.1.3 release

--- lib/icalendar/base.rb
+++ lib/icalendar/base.rb
@@ -9,7 +9,7 @@ require 'logger'
 
 module Icalendar #:nodoc:
 
-  VERSION = '1.1.2'
+  VERSION = '1.1.3'
     
   # A simple error class to differentiate iCalendar library exceptions
   # from ruby language exceptions or others.
