74884f7411aa052a0675d584840d043ace988070
Ryan Ahearn <ryan@coshx.com>
2013-03-31 22:09:15 -0400

    Switches expected/actual assert order

--- test/test_tzinfo.rb
+++ test/test_tzinfo.rb
@@ -14,14 +14,14 @@ class TestTZInfoExt < Test::Unit::TestCase
   def test_daylight_offset
     tz_offset_from = @timezone.instance_variable_get("@components")[:daylights][0].properties["tzoffsetfrom"]
     tz_offset_to = @timezone.instance_variable_get("@components")[:daylights][0].properties["tzoffsetto"]
-    assert_equal(tz_offset_from, "+0100")
-    assert_equal(tz_offset_to, "+0200")
+    assert_equal "+0100", tz_offset_from
+    assert_equal "+0200", tz_offset_to
   end
 
   def test_standard_offset
     tz_offset_from = @timezone.instance_variable_get("@components")[:standards][0].properties["tzoffsetfrom"]
-    tz_offset_to = @timezone.instance_variable_get("@components")[:daylights][0].properties["tzoffsetto"]
-    assert_equal(tz_offset_from, "+0200")
-    assert_equal(tz_offset_to, "+0200")
+    tz_offset_to = @timezone.instance_variable_get("@components")[:standards][0].properties["tzoffsetto"]
+    assert_equal "+0200", tz_offset_from
+    assert_equal "+0100", tz_offset_to
   end
 end
