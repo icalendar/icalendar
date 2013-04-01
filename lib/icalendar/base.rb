49ff59a472fdcca5596148fe6ec15e40197ce432
Ryan Ahearn <ryan@coshx.com>
2013-03-31 22:20:56 -0400

    Bumps version to 1.3.0

--- lib/icalendar/base.rb
+++ lib/icalendar/base.rb
@@ -11,7 +11,7 @@ require 'logger'
 
 module Icalendar #:nodoc:
 
-  VERSION = '1.2.4'
+  VERSION = '1.3.0'
 
   # A simple error class to differentiate iCalendar library exceptions
   # from ruby language exceptions or others.
