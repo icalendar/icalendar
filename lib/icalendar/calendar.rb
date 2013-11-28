d55a2164ccc9b2c9b227a3073bf5518770041784
Jake Craige <jake@poeticsystems.com>
2013-11-28 12:22:13 -0600

    Change respond_to? to respond_to_missing?

--- lib/icalendar/calendar.rb
+++ lib/icalendar/calendar.rb
@@ -47,7 +47,7 @@ module Icalendar
       end
     end
 
-    def respond_to?(method_name, include_private = false)
+    def respond_to_missing?(method_name, include_private = false)
       method_name.to_s =~ CAL_EXTENSION_REGEX || super
     end
 
