4a48145dc07eee7d4eb0ab03890bb05cd0aff26b
Ryan Ahearn <ryan@coshx.com>
2014-02-09 23:08:24 -0500

    Port tzinfo stuff from version 1

---
+++ lib/icalendar/tzinfo.rb
@@ -0,0 +1,140 @@
+=begin
+  Copyright (C) 2008 Sean Dague
+
+  This library is free software; you can redistribute it and/or modify it
+  under the same terms as the ruby language itself, see the file COPYING for
+  details.
+=end
+
+# The following adds a bunch of mixins to the tzinfo class, with the
+# intent on making it very easy to load in tzinfo data for generating
+# ical events.  With this you can do the following:
+#
+#   require "icalendar/tzinfo"
+#
+#   estart = DateTime.new(2008, 12, 29, 8, 0, 0)
+#   eend = DateTime.new(2008, 12, 29, 11, 0, 0)
+#   tstring = "America/Chicago"
+#
+#   tz = TZInfo::Timezone.get(tstring)
+#   cal = Calendar.new
+#   # the mixins now generate all the timezone info for the date in question
+#   timezone = tz.ical_timezone(estart)
+#   cal.add(timezone)
+#  
+#   cal.event do
+#       dtstart       estart
+#       dtend        eend
+#       summary     "Meeting with the man."
+#       description "Have a long lunch meeting and decide nothing..."
+#       klass       "PRIVATE"
+#   end
+#
+#   puts cal.to_ical
+#
+# The recurance rule calculations are hacky, and only start at the
+# beginning of the current dst transition.  I doubt this works for non
+# dst areas yet.  However, for a standard dst flipping zone, this
+# seems to work fine (tested in Mozilla Thunderbird + Lightning).
+# Future goal would be making this better.
+
+
+require 'tzinfo'
+
+module TZInfo
+  class Timezone
+    def ical_timezone(date, dst = Timezone.default_dst)
+      period = period_for_local(date, dst)
+      timezone = Icalendar::Timezone.new
+      timezone.tzid = identifier
+      if period.start_transition.nil?
+        timezone.add_component period.single
+      elsif period.end_transition.nil?
+        timezone.add_component period.dst? ? period.daylight : period.standard
+      else
+        timezone.add_component period.daylight
+        timezone.add_component period.standard
+      end
+      timezone
+    end
+  end
+
+  class TimezoneTransitionInfo
+    def offset_from
+      previous_offset.ical_offset
+    end
+
+    def offset_to
+      offset.ical_offset
+    end
+
+    def rrule 
+      start = local_start.to_datetime
+      # this is somewhat of a hack, but seems to work ok
+      [sprintf(
+        'FREQ=YEARLY;BYMONTH=%d;BYDAY=%d%s',
+        start.month,
+        ((start.day - 1)/ 7).to_i + 1,
+        start.strftime('%a').upcase[0,2]
+      )]
+    end
+
+    def dtstart
+      local_start.to_datetime.strftime '%Y%m%dT%H%M%S'
+    end
+  end
+
+  class TimezoneOffsetInfo
+    def ical_offset
+      o = utc_total_offset
+      sprintf '%+-2.2d%2.2d', (o / 3600).to_i, ((o / 60) % 60).to_i
+    end
+  end
+
+  class TimezonePeriod
+    def daylight
+      Icalendar::Timezone::Daylight.new.tap do |day|
+        if dst?
+          day.tzname = abbreviation.to_s
+          day.tzoffsetfrom = start_transition.offset_from
+          day.tzoffsetto = start_transition.offset_to
+          day.dtstart = start_transition.dtstart
+          day.rrule = start_transition.rrule unless end_transition.nil?
+        else
+          day.tzname = abbreviation.to_s.sub("ST","DT")
+          day.tzoffsetfrom = end_transition.offset_from
+          day.tzoffsetto = end_transition.offset_to
+          day.dtstart = end_transition.dtstart
+          day.rrule = end_transition.rrule
+        end
+      end
+    end
+
+    def standard
+      Icalendar::Timezone::Standard.new.tap do |std|
+        if dst?
+          std.tzname = abbreviation.to_s.sub('DT', 'ST')
+          std.tzoffsetfrom = end_transition.offset_from
+          std.tzoffsetto = end_transition.offset_to
+          std.dtstart = end_transition.dtstart
+          std.rrule = end_transition.rrule
+        else
+          std.tzname = abbreviation.to_s
+          std.tzoffsetfrom = start_transition.offset_from
+          std.tzoffsetto = start_transition.offset_to
+          std.dtstart = start_transition.dtstart
+          std.rrule = start_transition.rrule unless end_transition.nil?
+        end
+      end
+    end
+
+    def single
+      Icalendar::Timezone::Standard.new.tap do |std|
+        std.tzname = abbreviation.to_s
+        std.tzoffsetfrom = offset.ical_offset
+        std.tzoffsetto = offset.ical_offset
+        std.dtstart = DateTime.new(1970).strftime '%Y%m%dT%H%M%S'
+      end
+    end
+  end
+end
---
+++ spec/tzinfo_spec.rb
@@ -0,0 +1,85 @@
+require 'spec_helper'
+require_relative '../lib/icalendar/tzinfo'
+
+describe 'TZInfo::Timezone' do
+
+  let(:tz) { TZInfo::Timezone.get 'Europe/Copenhagen' }
+  let(:date) { DateTime.new 1970 }
+  subject { tz.ical_timezone date }
+
+  describe 'daylight offset' do
+    specify { expect(subject.daylights.first.tzoffsetto.value_ical).to eq "+0200" }
+    specify { expect(subject.daylights.first.tzoffsetfrom.value_ical).to eq "+0100" }
+  end
+
+  describe 'standard offset' do
+    specify { expect(subject.standards.first.tzoffsetto.value_ical).to eq "+0100" }
+    specify { expect(subject.standards.first.tzoffsetfrom.value_ical).to eq "+0200" }
+  end
+
+  describe 'no end transition' do
+    let(:tz) { TZInfo::Timezone.get 'America/Cayman' }
+    let(:date) { DateTime.now }
+
+    it 'only creates a standard component' do
+      expect(subject.to_ical).to eq <<-EXPECTED.gsub "\n", "\r\n"
+BEGIN:VTIMEZONE
+TZID:America/Cayman
+BEGIN:STANDARD
+DTSTART:19120201T000711
+TZOFFSETTO:-0500
+TZOFFSETFROM:-0652
+TZNAME:EST
+END:STANDARD
+END:VTIMEZONE
+      EXPECTED
+    end
+  end
+
+  describe 'no transition' do
+    let(:tz) { TZInfo::Timezone.get 'UTC' }
+    let(:date) { DateTime.now }
+
+    it 'creates a standard component with equal offsets' do
+      expect(subject.to_ical).to eq <<-EXPECTED.gsub "\n", "\r\n"
+BEGIN:VTIMEZONE
+TZID:UTC
+BEGIN:STANDARD
+DTSTART:19700101T000000
+TZOFFSETTO:+0000
+TZOFFSETFROM:+0000
+TZNAME:UTC
+END:STANDARD
+END:VTIMEZONE
+      EXPECTED
+    end
+  end
+
+  describe 'dst transition' do
+    subject { TZInfo::Timezone.get 'America/Los_Angeles' }
+    let(:now) { subject.now }
+    # freeze in DST transition in America/Los_Angeles
+    before(:each) { Timecop.freeze '2013-11-03T01:30:00-08:00' }
+    after(:each) { Timecop.return }
+
+    specify { expect { subject.ical_timezone now, nil }.to raise_error TZInfo::AmbiguousTime }
+    specify { expect { subject.ical_timezone now, true }.not_to raise_error }
+    specify { expect { subject.ical_timezone now, false }.not_to raise_error }
+
+    context 'TZInfo::Timezone.default_dst = nil' do
+      before(:each) { TZInfo::Timezone.default_dst = nil }
+      specify { expect { subject.ical_timezone now }.to raise_error TZInfo::AmbiguousTime }
+    end
+
+    context 'TZInfo::Timezone.default_dst = true' do
+      before(:each) { TZInfo::Timezone.default_dst = true }
+      specify { expect { subject.ical_timezone now }.not_to raise_error }
+    end
+
+    context 'TZInfo::Timezone.default_dst = false' do
+      before(:each) { TZInfo::Timezone.default_dst = false }
+      specify { expect { subject.ical_timezone now }.not_to raise_error }
+    end
+  end
+
+end
