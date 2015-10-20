317754a39a999df1bda41559a041a192dd3fbac9
arthurva <arthur@mobilize.io>
2015-10-21 01:06:12 +0300

    add support to X-WR-CALNAME and X-WR-CALDESC properties

--- lib/icalendar/calendar.rb
+++ lib/icalendar/calendar.rb
@@ -5,6 +5,9 @@ module Icalendar
     required_property :prodid
     optional_single_property :calscale
     optional_single_property :ip_method
+    optional_single_property :x_wr_calname
+    optional_single_property :x_published_ttl
+    optional_single_property :x_wr_caldesc
 
     component :timezone, :tzid
     component :event
@@ -17,6 +20,9 @@ module Icalendar
       self.prodid = 'icalendar-ruby'
       self.version = '2.0'
       self.calscale = 'GREGORIAN'
+      self.x_wr_calname = nil
+      self.x_published_ttl = nil
+      self.x_wr_caldesc = nil
     end
 
     def publish
