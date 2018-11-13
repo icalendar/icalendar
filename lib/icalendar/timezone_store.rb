56d97e03eca321f73aa2af3dcf95172ca2c22097
Ryan Ahearn <ryan.c.ahearn@gmail.com>
2018-11-13 16:07:03 -0500

    Add deprecation warnings to other TimezoneStore class methods

--- lib/icalendar/timezone_store.rb
+++ lib/icalendar/timezone_store.rb
@@ -14,10 +14,12 @@ module Icalendar
     end
 
     def self.store(timezone)
+      warn "**** DEPRECATION WARNING ****\nTimezoneStore.store will be removed in 3.0. Please use instance methods."
       instance.store timezone
     end
 
     def self.retrieve(tzid)
+      warn "**** DEPRECATION WARNING ****\nTimezoneStore.retrieve will be removed in 3.0. Please use instance methods."
       instance.retrieve tzid
     end
 
