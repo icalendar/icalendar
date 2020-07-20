516f1abf6d72389b3b82a866238ae4b713f2993f
Amol Pujari <amolpujari@gmail.com>
2020-07-20 20:10:28 +0530

    as per rog change

--- lib/icalendar/calendar.rb
+++ lib/icalendar/calendar.rb
@@ -5,6 +5,8 @@ module Icalendar
     required_property :prodid
     optional_single_property :calscale
     optional_single_property :ip_method
+    optional_single_property :x_wr_calname
+    optional_single_property :x_wr_timezone
 
     component :timezone, :tzid
     component :event
@@ -12,11 +14,13 @@ module Icalendar
     component :journal
     component :freebusy
 
-    def initialize
+    def initialize(name = nil, timezone = nil)
       super 'calendar'
       self.prodid = 'icalendar-ruby'
       self.version = '2.0'
       self.calscale = 'GREGORIAN'
+      self.x_wr_calname = name if name
+      self.x_wr_timezone = timezone if timezone
     end
 
     def publish
