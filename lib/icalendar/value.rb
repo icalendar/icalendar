1bddb2321bb24698086c83d43fa2f9dd94e6c477
Thomas Cannon <tcannon00@gmail.com>
2023-02-23 11:56:47 -0500

    Add `# frozen_string_literal: true` to library code for performance hint

--- lib/icalendar/value.rb
+++ lib/icalendar/value.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+
 require 'delegate'
 require 'icalendar/downcased_hash'
 
--- lib/icalendar/values/text.rb
+++ lib/icalendar/values/text.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Icalendar
   module Values
     class Text < Value
